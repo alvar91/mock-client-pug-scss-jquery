8dcfad098ce5400c398311e75c5c1bc19572c6b2
alvar91 <aleksey91scorp@bk.ru>
2021-03-17 11:27:58 +0300

    fix: disable scroll when modal is open

--- js/popup/popup.js
+++ js/popup/popup.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
   const jsForm = $(".js-form");
   $(".js-popup").click(function () {
+    document.body.style.overflow = 'hidden';
     jsForm.fadeIn(300);
   });
 
   $(".js-form-close").click(function () {
+    document.body.style.overflow = 'auto';
     jsForm.fadeOut(300);
   });
 
   jsForm.click(function (event) {
     if (!event.target.closest(".js-content")) {
+      document.body.style.overflow = 'auto';
       jsForm.fadeOut(300);
     }
   });
