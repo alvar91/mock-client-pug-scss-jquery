729a91b2d513d494f7ee0359a7be08d5cbde8447
alvar91 <aleksey91scorp@bk.ru>
2021-03-17 15:17:26 +0300

    fix: fixed validation error titles

--- js/popup/popup-call.js
+++ js/popup/popup-call.js
@@ -1,21 +1,19 @@
 $(document).ready(function () {
-    const jsForm = $(".js-form-call");
-    $(".js-popup-call").click(function () {
-      document.body.style.overflow = 'hidden';
-      jsForm.fadeIn(300);
-
-    });
-  
-    $(".js-form-close-call").click(function () {
-      document.body.style.overflow = 'auto';
-      jsForm.fadeOut(300);
-    });
-  
-    jsForm.click(function (event) {
-      if (!event.target.closest(".js-content-call")) {
-        document.body.style.overflow = 'auto';
-        jsForm.fadeOut(300);
-      }
-    });
+  const jsForm = $(".js-form-call");
+  $(".js-popup-call").click(function () {
+    document.body.style.overflow = "hidden";
+    jsForm.fadeIn(300);
   });
-  
+
+  $(".js-form-close-call").click(function () {
+    document.body.style.overflow = "auto";
+    jsForm.fadeOut(300);
+  });
+
+  jsForm.click(function (event) {
+    if (!event.target.closest(".js-content-call")) {
+      document.body.style.overflow = "auto";
+      jsForm.fadeOut(300);
+    }
+  });
+});
